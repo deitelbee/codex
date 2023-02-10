ca9bb422324c8f48c4c347cd29266589ece8597c
deitelbee <409374588@qq.com>
2023-02-11 00:29:34 +0800

    update url

--- client/script.js
+++ client/script.js
@@ -71,7 +71,7 @@ const handleSubmit = async (e) => {
 
   const messageDiv = document.getElementById(uniqueId)
   loader(messageDiv);
-  const response = await fetch("http://127.0.0.1:5000",{
+  const response = await fetch("https://codex-9msg.onrender.com",{
     method:'POST',
     headers: {
       'Content-Type':'application/json'
